ee147b69d3874d8dcd5c706a34635f1ef042a76c
QUBITABHAY <codingcontestabhay@gmail.com>
2026-01-18 16:07:05 +0530

    feat: add input feild component

---
+++ docs/components.md
@@ -0,0 +1,8 @@
+# Components
+
+## InputFeild
+
+- title: string
+- placeholder?: string
+- maxLength?: number
+
---
+++ src/components/common/InputFeild.tsx
@@ -0,0 +1,25 @@
+interface InputFeildProps {
+  title: string;
+  placeholder?: string;
+  maxLength?: number;
+}
+
+function InputFeild({ title, placeholder, maxLength }: InputFeildProps) {
+  return (
+    <div className="flex flex-col">
+      <label
+        className="text-sm font-medium text-gray-900"
+      >
+        {title}
+      </label>
+      <input
+        type="text"
+        className="mt-1 p-2 border-b border-gray-300 focus:outline-none focus:border-blue-500"
+        placeholder={placeholder}
+        maxLength={maxLength}
+      />
+    </div>
+  )
+}
+
+export default InputFeild;
